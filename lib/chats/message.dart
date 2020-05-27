79e85122db0039ad8994081fd763ff1333928933
Tuksa <emmydave414@gmail.com>
2020-05-27 18:00:52 +0100

    chat fix

--- lib/chats/message.dart
+++ lib/chats/message.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:bubble/bubble.dart';
 import 'package:peddazz/colors.dart';
 
 class Message extends StatelessWidget {
@@ -14,54 +15,77 @@ class Message extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+//      child: Column(
+//        crossAxisAlignment:
+//            person ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+//        children: <Widget>[
+////          Padding(
+////            padding: const EdgeInsets.only(left: 5, right: 5),
+////            child: Text(
+////              from,
+////              style: TextStyle(color: Colors.grey),
+////            ),
+////          ),
+//          Padding(
+//            padding:
+//            person ? const EdgeInsets.only(
+//              left: 50,
+//            right: 10,
+//            bottom: 10
+//            ) : const EdgeInsets.only(
+//              left: 10,
+//              right: 50,
+//              bottom: 10
+//              ),
+//            child: Material(
+//              color: person ? AppColor.login1 : AppColor.appBar,
+//              borderRadius: person ? BorderRadius.only(
+//                topLeft: Radius.circular(10),
+//                topRight: Radius.circular(10),
+//                bottomLeft: Radius.circular(10)
+//              ) : BorderRadius.only(
+//                  topLeft: Radius.circular(10),
+//                  topRight: Radius.circular(10),
+//                  bottomRight: Radius.circular(10)
+//              ) ,
+//              elevation: 2.0,
+//              child: Container(
+//                //width: MediaQuery.of(context).size.width*0.75,
+//                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+//                child: Text(
+//                  text,
+//                  style: TextStyle(
+//                    color: Colors.white
+//                  ),
+//                  ),
+//              ),
+//            ),
+//          )
+//        ],
+//      ),
+    child: Container(
       child: Column(
         crossAxisAlignment:
-            person ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        person ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: <Widget>[
-//          Padding(
-//            padding: const EdgeInsets.only(left: 5, right: 5),
-//            child: Text(
-//              from,
-//              style: TextStyle(color: Colors.grey),
-//            ),
-//          ),
-          Padding(
-            padding:
-            person ? const EdgeInsets.only(
-              left: 50,
-            right: 10,
-            bottom: 10
-            ) : const EdgeInsets.only(
-              left: 10,
-              right: 50,
-              bottom: 10
-              ),
-            child: Material(
-              color: person ? AppColor.login1 : AppColor.appBar,
-              borderRadius: person ? BorderRadius.only(
-                topLeft: Radius.circular(10),
-                topRight: Radius.circular(10),
-                bottomLeft: Radius.circular(10)
-              ) : BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(10),
-                  bottomRight: Radius.circular(10)
-              ) ,
-              elevation: 2.0,
-              child: Container(
-                //width: MediaQuery.of(context).size.width*0.75,
-                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-                child: Text(
-                  text,
-                  style: TextStyle(
-                    color: Colors.white
-                  ),
-                  ),
+          Bubble(
+            color: person ? AppColor.login1 : AppColor.appBar,
+            margin: BubbleEdges.only(
+                top: 10,
+              right: person ? 10 : 0,
+              left: person ? 0 : 10
+            ),
+            nip: person ? BubbleNip.rightTop : BubbleNip.leftTop,
+            child: Text(
+              text,
+              style: TextStyle(
+                color: Colors.white
               ),
             ),
           )
         ],
       ),
+    ),
     );
   }
 }
